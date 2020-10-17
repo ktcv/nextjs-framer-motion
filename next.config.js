8a2068ae40b62768b1bc6b7584b5ff5e7baa14c9
Kevin Chen <ktchenvideo@gmail.com>
2020-10-18 08:21:53 +1100

    Update travis.yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
-  basePath: '/nextjs-framer-motion',
-  assetPrefix: '/nextjs-framer-motion/',
+  // basePath: '/nextjs-framer-motion',
+  // assetPrefix: '/nextjs-framer-motion/',
 })
